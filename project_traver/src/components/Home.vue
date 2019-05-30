fa6f84b845ab5f17722c43b6f79c5731caf9f54f
小软蛋 <1204628226@qq.com>
2019-05-30 18:10:17 +0800

    travel
    header的操作，更多是关于css的学习

---
+++ project_traver/src/components/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <home-header></home-header>
+</template>
+
+<script>
+import HomeHeader from './Header.vue'
+export default {
+  name: 'Home',
+  components:{
+    HomeHeader
+  }
+  
+};
+</script>
+
+
+<style scoped>
+</style>
