18a2ed39f74911fb2a51b24cc424b1f3676c9d83
小软蛋 <1204628226@qq.com>
2020-04-24 20:57:17 +0800

    项目启动的第一天，做相关准备

---
+++ vue-sell-cube/vue.config.js
@@ -0,0 +1,44 @@
+const appDate = require('./data.json')
+const seller = appDate.seller
+const goods = appDate.goods
+const ratings = appDate.ratings
+module.exports = {
+  css: {
+    loaderOptions: {
+      stylus: {
+        'resolve url': true,
+        import: [
+          './src/theme'
+        ]
+      }
+    }
+  },
+  pluginOptions: {
+    'cube-ui': {
+      postCompile: true,
+      theme: true
+    }
+  },
+  devServer: {
+    before(app) {
+      app.get('/api/seller', function (req, res) {
+        res.json({
+          errno: 0,
+          data: seller
+        })
+      })
+      app.get('/api/goods', function (req, res) {
+        res.json({
+          errno: 0,
+          data: goods
+        })
+      })
+      app.get('/api/ratings', function (req, res) {
+        res.json({
+          errno: 0,
+          data: ratings
+        })
+      })
+    }
+  }
+}
