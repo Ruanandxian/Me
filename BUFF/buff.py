28b4c085fb2234358b6fbd137c5856adba822537
小软蛋 <35754651+Ruanandxian@users.noreply.github.com>
2020-06-04 15:19:17 +0800

    Update buff.py

--- BUFF/buff.py
+++ BUFF/buff.py
@@ -25,7 +25,6 @@ from io import BytesIO
 
 
 username = '17737102596'
-passwd = 'woaixiaoruan2728'
 mail_host = "smtp.163.com"  # 设置服务器
 mail_user = "myperi2728@163.com"  # 用户名
 mail_pass = "woaixiaoruan0913"  # 口令,QQ邮箱是输入授权码，在qq邮箱设置 里用验证过的手机发送短信获得，不含空格
